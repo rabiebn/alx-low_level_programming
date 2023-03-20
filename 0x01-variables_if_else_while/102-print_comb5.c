eaca4ec7695983e8f5e870c09e9be660f5b5abce
rabiebn <rabie.bnmr@gmail.com>
2023-03-20 06:59:59 -0700

    Fix4 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,34 +10,22 @@ int main(void)
 {
 	int tens;
 	int ones;
-	int t;
-	int o;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+	for (tens = 0; tens < 100; tens++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (ones = 0; ones < 100; ones++)
 		{
-			for (t = tens; t <= '9'; t++) /*print second of pair*/
+			if (n < m)
 			{
-				for (o = ones; o <= '9'; o++)
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				putchar(' ');
+				putchar((m / 10) + 48);
+				putchar((m % 10) + 48);
+				if (n != 98 || m != 99)
 				{
-					if (tens == t && ones == o)
-					{
-						continue;
-					}
-					else
-					{
-						putchar(tens);
-						putchar(ones);
-						putchar(' ');
-						putchar(t);
-						putchar(o);
-					}
-					if (!((tens == '9' && ones == '8') && (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
