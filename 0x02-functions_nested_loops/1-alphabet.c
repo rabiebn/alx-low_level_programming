d35bf1d4d514ad2b5c034f69ac3ffebbe1a2b90c
rabiebn <rabie.bnmr@gmail.com>
2023-03-21 16:04:06 -0700

    Initial commit 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet()
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	return;
+}
+int main(void)
+{
+	return (0);
+}
