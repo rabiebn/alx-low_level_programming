86c4d18941f6bb9fff5000f5fdc6cb807fa5824c
rabiebn <rabie.bnmr@gmail.com>
2023-03-31 17:53:04 -0700

    Second commit advanced 101 102

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer
+ */
+
+void print_number(int n)
+{
+	unsigned int tens, digit, intPos = n;
+	unsigned int digitNum = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			intPos = n * -1;
+			_putchar('-');
+		}
+		while (digitNum <= intPos)
+			digitNum *= 10;
+		tens = digitNum / 10;
+
+		while (tens >= 1)
+		{
+			digit = intPos / tens;
+			_putchar(digit + '0');
+			intPos = (intPos - (tens * digit));
+			tens /= 10;
+		}
+	}
+}
