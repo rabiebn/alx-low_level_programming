b2e7621a60e65abf5f4e454980dc2f92ebb60b38
rabiebn <rabie.bnmr@gmail.com>
2023-04-11 13:58:57 -0700

    Fix 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,9 @@ char *create_array(unsigned int size, char c)
 	char *p = (char *) malloc(sizeof(char) * size);
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	if (p == NULL)
 		return (NULL);
 
