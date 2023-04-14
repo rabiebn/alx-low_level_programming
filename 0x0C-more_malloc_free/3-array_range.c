bfe9320a65cccc2729fcae8cf14e9268ccbb0ca4
rabiebn <rabie.bnmr@gmail.com>
2023-04-13 18:29:31 -0700

    Fix 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i <= (max - min + 1); i++)
+	for (i = 0; i < (max - min + 1); i++)
 		*(p + i) = min++;
 
 	return (p);
