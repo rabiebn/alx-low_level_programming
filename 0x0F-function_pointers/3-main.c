76a9e1ad9c8cc34ce805d6c22f97b9623943b76b
rabiebn <rabie.bnmr@gmail.com>
2023-04-20 16:08:43 -0700

    Fix 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,7 +12,6 @@
 int main(int ac, char **av)
 {
 	int n1, n2;
-	int (*f)(int, int);
 
 	if (ac != 4)
 	{
@@ -21,9 +20,8 @@ int main(int ac, char **av)
 	}
 	n1 = atoi(av[1]);
 	n2 = atoi(av[3]);
-	f = get_op_func(av[2]);
 
-	if (f == NULL)
+	if (!get_op_func(av[2]))
 	{
 		printf("Error\n");
 		exit(99);
