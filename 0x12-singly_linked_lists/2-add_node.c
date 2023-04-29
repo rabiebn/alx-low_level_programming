71125a0ca3647bc8718d97c6c27a9cb2694ef58f
rabiebn <rabie.bnmr@gmail.com>
2023-04-29 03:04:17 -0700

    Fixed 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,13 +10,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_head = malloc(sizeof(list_t));
 
-	new_head->str = malloc(sizeof(char) * (strlen(str) + 1));
-	if (!new_head || !new_head->str)
+	if (!new_head)
 		return (NULL);
 
 	new_head->str = strdup(str);
 	new_head->len = strlen(str);
 	new_head->next = *head;
+	*head = new_head;
 
-	return (new_head);
+	return (*head);
 }
