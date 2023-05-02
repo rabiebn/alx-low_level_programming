218ef2b04d5c76d9dc164c4818f5a73dbf6732e1
rabiebn <rabie.bnmr@gmail.com>
2023-05-02 16:53:57 -0700

    re-write 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,32 +1,6 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - finds and returns the n-th node a listint_t list
- * @head: head of the listint_t list
- * @index: index of node starting at 0
- *
- * Return: node according to the index, NULL if doesn't exist.
- */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
-	listint_t *tmp;
-	unsigned int i = 0;
-
-	if (!head)
-		return (NULL);
-
-	tmp = head;
-	while (i < index)
-	{
-		if (!tmp)
-			return (NULL);
-		tmp = tmp->next;
-		i++;
-	}
-	return (tmp);
-}
-
-/**
  * delete_nodeint_at_index - deletes the node at index of a listint_t list
  * @head: head of the list
  * @index: index of the node to delete
@@ -35,25 +9,34 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+
+	unsigned int i = 0;
 	listint_t *tmp, *tmp2;
 
-	if (!*head)
-		return (1);
-
-	tmp = get_nodeint_at_index(*head, index);
-	if (!tmp)
+	if (*head == NULL)
 		return (-1);
 
-	free(tmp);
+	tmp = *head;
+
 	if (index == 0)
 	{
-		free(*head);
-		*head = (*head)->next;
+		*head = tmp->next;
+		free(tmp);
 		return (1);
 	}
 
-	tmp = get_nodeint_at_index(*head, index - 1);
-	tmp2 = get_nodeint_at_index(*head, index + 1);
-	tmp->next = tmp2;
+	while (i < (index - 1) && tmp != NULL)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (i != (index - 1) || tmp->next == NULL)
+		return (-1);
+
+	tmp2 = tmp->next;
+	tmp->next = (tmp->next)->next;
+	free(tmp2);
+
 	return (1);
 }
