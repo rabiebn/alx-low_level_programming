5c33e1678dddb87f2c615548d0da47c680bba3a9
rabiebn <rabie.bnmr@gmail.com>
2023-05-02 17:50:08 -0700

    Fix 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -66,8 +66,7 @@ char **strtow(char *str)
 			found_word = str;
 			for (; *str != ' ' && *str != '\0';)
 			{
-				length++;
-				str++;
+				length++, str++;
 			}
 			words[b] = malloc((length + 1) * sizeof(char));
 			if (words[b] == 0)
@@ -82,10 +81,8 @@ char **strtow(char *str)
 				c++;
 			}
 			words[b][c] = '\0';
-			b++;
-			c = 0;
-			length = 0;
-			str++;
+			b++, str++;
+			c = 0, length = 0;
 		}
 	}
 	return (words);
