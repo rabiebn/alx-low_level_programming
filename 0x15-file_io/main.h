4a38c597863f94af245267c00611c137662068a5
rabiebn <rabie.bnmr@gmail.com>
2023-05-27 06:40:14 -0700

    advanced tasks

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,23 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* dprintf */
-#include <stdio.h>
+#define BUFFER_SIZE 1024
+#define FILE_ERROR -1
+#define INVALID_NUMBER_OF_ARGUMENTS 97
+#define DOES_NOT_EXIST_OR_UNABLE_TO_READ 98
+#define CANNOT_CREATE_OR_WRITE 99
+#define CANNOT_CLOSE 100
+#define ARGUMENT_COUNT_EXPECTED 3
 
-/* malloc, free */
+#include <elf.h>
+#include <errno.h>
 #include <stdlib.h>
-
-/* close */
+#include <stdio.h>
 #include <unistd.h>
-
-/* open */
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 
 #endif
