28b2bb819a672d5f915f60593be756b262587839
rabiebn <rabie.bnmr@gmail.com>
2023-07-06 15:23:39 -0700

    0: set array to NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,15 +8,26 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i = 0;
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_node_t **array;
 
 	if (!ht)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t) * size);
-	if (!ht->array)
+	array = malloc(sizeof(hash_node_t) * size);
+	if (!array)
+	{
+		free(ht);
 		return (NULL);
+	}
+	while (i <size)
+	{
+		array[i] = NULL;
+		i++;
+	}
+	ht->size = size;
+	ht->array = array;
 
 	return (ht);
 }
