5e70657322ebf37adbc49a37bc19cfe281cfe6ad
rabiebn <rabie.bnmr@gmail.com>
2023-07-08 08:20:00 -0700

    1: Initial commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,23 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_table_t *ht;
 	hash_node_t **array;
+	
+	if (size == 0)
+		return (NULL);
 
+	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t) * size);
+	array = malloc(sizeof() * size);
 	if (!array)
 	{
 		free(ht);
 		return (NULL);
 	}
-	while (i <size)
+	while (i < size)
 	{
 		array[i] = NULL;
 		i++;
