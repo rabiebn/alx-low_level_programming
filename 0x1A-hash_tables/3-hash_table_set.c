365afb49e8c6819d2f547ecd04583802195d75b2
rabiebn <rabie.bnmr@gmail.com>
2023-07-08 16:06:57 -0700

    3: handled the case where hash table is NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,26 +11,27 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index, size = ht->size;
+	unsigned long int index, size;
 	hash_node_t *tmp, *new_node, **array;
 
+	if (!ht)
+		return (0);
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
 
+	size = ht->size;
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-
 	if (!new_node->key || !new_node->value)
 	{
 		free(new_node);
 		return (0);
 	}
 	index = key_index((unsigned char *)new_node->key, size);
-
 	array = ht->array;
 	tmp = array[index];
-
 	while (tmp)
 	{
 		if (strcmp(tmp->key, new_node->key) == 0)
@@ -46,7 +47,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		array[index] = new_node;
 		return (1);
 	}
-
 	new_node->next = array[index];
 	array[index] = new_node;
 
