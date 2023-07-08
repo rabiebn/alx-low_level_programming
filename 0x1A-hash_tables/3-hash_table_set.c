80ac26d63599ecb8c67efa854db1f02c48cdcf71
rabiebn <rabie.bnmr@gmail.com>
2023-07-08 10:51:21 -0700

    3: update value if key already exists

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,31 +13,33 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index, size = ht->size;
 	hash_node_t *tmp, *new_node, **array;
-	char *key_dup, *value_dup;
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
 
-	key_dup = strdup(key);
-	value_dup = strdup(value);
-	if (!key_dup || !value_dup)
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	if (!new_node->key || !new_node->value)
 	{
 		free(new_node);
 		return (0);
 	}
-	index = key_index((unsigned char *)key_dup, size);
+	index = key_index((unsigned char *)new_node->key, size);
 
 	array = ht->array;
 	tmp = array[index];
+
 	while (tmp)
 	{
-		if (strcmp(tmp->key, key_dup) == 0)
-			return (0);
+		if (strcmp(tmp->key, new_node->key) == 0)
+		{
+			tmp->value = new_node->value;
+			return (1);
+		}
 		tmp = tmp->next;
 	}
-	new_node->key = key_dup;
-	new_node->value = value_dup;
 	if (!array[index])
 	{
 		new_node->next = NULL;
