12a090ba18708fe581125bc984d4fdaa2d47a0ff
rabiebn <rabie.bnmr@gmail.com>
2023-07-08 08:30:33 -0700

    0: malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!ht)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t) * size);
+	array = malloc(sizeof(*array) * size);
 	if (!array)
 	{
 		free(ht);
