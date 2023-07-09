4e4e9862ef257cd936a02d50ed1018988220e159
rabiebn <rabie.bnmr@gmail.com>
2023-07-08 17:16:19 -0700

    5: Initial commit

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,48 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: hash table.
+ *
+ * Return: Void.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node;
+
+	if (!ht)
+	{
+		printf("{}\n");
+		return;
+	}
+	node = ht->array[i];
+	while (!node && (i < ht->size)) /* get the first not empty node */
+		node = ht->array[i++];
+	if (!node)
+	{
+		printf("{}\n");
+		return;
+	}
+	printf("{'%s': '%s'", node->key, node->value);
+	node = node->next;
+	while (node)
+	{
+		printf(", '%s': '%s'", node->key, node->value);
+		node = node->next;
+	}
+	for (; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		if (!node)
+			continue;
+		printf(", '%s': '%s'", node->key, node->value);
+		node = node->next;
+		while (node)
+		{
+			printf(", '%s': '%s'", node->key, node->value);
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
