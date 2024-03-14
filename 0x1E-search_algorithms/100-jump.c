f560ba09123ee3f413d43ddc9a1adf478ae2ac51
rabiebn <rabie.bnmr@gmail.com>
2024-03-14 22:11:41 +0000

    100: it didnt print the first compare

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -20,6 +20,8 @@ int jump_search(int *array, size_t size, int value)
 
 	step = sqrt(size);
 	prev = 0;
+    printf("Value checked array[%lu] = [%d]\n",
+				prev, *(array + prev));
 	while (*(array + step) < value)
 	{
 		printf("Value checked array[%lu] = [%d]\n",
