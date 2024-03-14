c577a8f2cddd6fba87fe8e724b3c009fecbd200b
rabiebn <rabie.bnmr@gmail.com>
2024-03-14 22:07:41 +0000

    100: Initial commit

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,9 +1,45 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
- * 
-*/
+ * jump_search - searches for a value in a sorted array of integers
+ *               using the Jump search algorithm
+ *
+ * @array: pointer to the first element of the sorted (asc) array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: first index where value is located,
+ *         -1 if value not in array and/or array is NULL.
+ */
 int jump_search(int *array, size_t size, int value)
 {
-    
+	size_t step, prev, i = 0;
+
+	if (!array)
+		return (-1);
+
+	step = sqrt(size);
+	prev = 0;
+	while (*(array + step) < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n",
+				step, *(array + step));
+		prev = step;
+		step += sqrt(size);
+		if (prev >= (size - 1))
+			return (-1);
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n", prev, step);
+
+	while (prev + i <= step)
+	{
+		printf("Value checked array[%lu] = [%d]\n",
+				prev + i, *(array + prev + i));
+		if (*(array + prev + i) == value)
+			return (prev + i);
+		i++;
+	}
+
+	return (-1);
 }
--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "search_algos.h"
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always EXIT_SUCCESS
  */
 int main(void)
 {
-	int array[17] = {
-		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 25, 48, 89
-	};
-	printf("Found at index: %d\n", binary_search(array, 16, 12));
-	return (0);
+    int array[] = {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n\n", 6, jump_search(array, size, 6));
+    printf("Found %d at index: %d\n\n", 1, jump_search(array, size, 1));
+    printf("Found %d at index: %d\n", 999, jump_search(array, size, 999));
+    return (EXIT_SUCCESS);
 }
